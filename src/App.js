40d583317fab923a169470d7eda3d7c6af765b84
nmccray <n.mccray@campus.tu-berlin.de>
2022-09-29 15:04:36 +0200

    initial push

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const basicOps = ["/","*","+","-","."];
+
+  const updateCalc = value => {
+    setCalc(calc + value);
+  }
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++){
+      digits.push(
+          <button key={i} onClick={() => updateCalc(i.toString())}>{i}</button>
+      )
+    }
+
+    return digits;
+  }
+
+  return (
+    <div className="App">
+      <div className={"calculator"}>
+        <div className={"display"}>
+          {result ? <span>(0)</span>: '' } {calc || "0"}
+        </div>
+        <div className={"operators"}>
+          <button onClick={() => updateCalc("/")}>+</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+          <button onClick={() => {}}>DEL</button>
+        </div>
+        <div className={"digits"}>
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
