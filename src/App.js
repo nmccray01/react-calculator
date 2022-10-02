a63b9b43764e836b632904dd204e1dafcda30b10
nmccray <n.mccray@campus.tu-berlin.de>
2022-10-02 08:11:43 +0200

    added more operators, pi and eulers

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import {useState} from "react";
 function App() {
   const [calc, setCalc] = useState("");
   const [result, setResult] = useState("");
+  const [display, setDisplay] = useState("");
 
-  const updateCalc = value => {
+  const updateInput = (value, repres) => {
     setCalc(calc + value);
+    setDisplay(display + repres);
   }
+
+
 
   const createDigits = () => {
     const digits = [];
 
     for (let i = 1; i < 10; i++){
       digits.push(
-          <button key={i} onClick={() => updateCalc(i.toString())}>{i}</button>
+          <button key={i} onClick={() => updateInput(i.toString(), i.toString())}>{i}</button>
       )
     }
 
@@ -23,17 +27,25 @@ function App() {
 
   const calculate = () => {
     const val = Function(`"use strict";return (${calc})`)();
-    setCalc(val);
-    setResult(val);
+    setCalc(val + "");
+    setResult(val + "");
+    setDisplay(val + "");
   }
 
   const deleteLast = () =>{
-    if(calc === ''){
-      return;
+    if(calc.endsWith("**")){
+      setCalc(calc.slice(0, -2));
+      setDisplay(display.slice(0, -1));
+    } else if (calc.endsWith(Math.PI.toFixed(10).toString())){
+      setCalc(calc.slice(0, -12));
+      setDisplay(display.slice(0, -1));
+    } else if (calc.endsWith(Math.E.toFixed(10).toString())){
+      setCalc(calc.slice(0, -12));
+      setDisplay(display.slice(0, -1));
+    } else {
+      setCalc(calc.slice(0, -1));
+      setDisplay(display.slice(0, -1));
     }
-
-    const value = calc.slice(0, -1);
-    setCalc(value);
   }
 
 
@@ -41,20 +53,36 @@ function App() {
     <div className="App">
       <div className={"calculator"}>
         <div className={"display"}>
-          {result ? <span>({result})</span>: '' } {calc || "0"}
+          {result ? <span>({result})</span>: '' } {display || "0"}
         </div>
         <div className={"operators"}>
-          <button onClick={() => updateCalc("+")}>+</button>
-          <button onClick={() => updateCalc("-")}>-</button>
-          <button onClick={() => updateCalc("*")}>*</button>
-          <button onClick={() => updateCalc("/")}>/</button>
-          <button onClick={() => {updateCalc(result)}}>ANS</button>
+          <button onClick={() => {}}>log</button>
+          <button onClick={() => {}}>ln</button>
+          <button onClick={() => {}}>sin</button>
+          <button onClick={() => {}}>cos</button>
+          <button onClick={() => {}}>tan</button>
+          <button onClick={() => updateInput(Math.E.toFixed(10), String.fromCharCode(8455))}>&#8455;</button>
+        </div>
+        <div className={"operators"}>
+          <button onClick={() => updateInput(Math.PI.toFixed(10), String.fromCharCode(960))}>&#960;</button>
+          <button onClick={() => updateInput("**", "^")}>^</button>
+          <button onClick={() => updateInput("(", "(")}>(</button>
+          <button onClick={() => updateInput(")", ")")}>)</button>
+          <button onClick={() => updateInput("%", "%")}>mod</button>
+          <button onClick={() => updateInput("**0.5", "^0.5")}>sqrt</button>
+        </div>
+        <div className={"operators"}>
+          <button onClick={() => updateInput("+", "+")}>+</button>
+          <button onClick={() => updateInput("-", "-")}>-</button>
+          <button onClick={() => updateInput("*", "*")}>*</button>
+          <button onClick={() => updateInput("/", "/")}>/</button>
+          <button onClick={() => {updateInput(result, result)}}>ANS</button>
           <button onClick={() => {deleteLast()}}>DEL</button>
         </div>
         <div className={"digits"}>
           {createDigits()}
-          <button onClick={() => updateCalc("0")}>0</button>
-          <button onClick={() => updateCalc(".")}>.</button>
+          <button onClick={() => updateInput("0", "0")}>0</button>
+          <button onClick={() => updateInput(".", ".")}>.</button>
           <button onClick={() => calculate()}>=</button>
         </div>
       </div>
